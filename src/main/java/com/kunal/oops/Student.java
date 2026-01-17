5f2cdf8484db963abf47d0941ab96b1a78b0f9ab
Voldy13 <136642925+Voldemond@users.noreply.github.com>
2026-01-17 22:23:23 +0530

    Extending Class with Inheritance

---
+++ src/main/java/com/kunal/oops/Student.java
@@ -0,0 +1,12 @@
+package main.java.com.kunal.oops;
+
+public class Student extends User   {
+
+    private boolean _verified = false ;
+    void set_verified(boolean verified){
+        _verified = verified;
+    }
+    boolean get_verified(){
+        return _verified;
+    }
+}
--- src/main/java/com/kunal/oops/oop.java
+++ src/main/java/com/kunal/oops/oop.java
@@ -5,11 +5,19 @@ import java.util.List;
 
 public class oop {
     public static void main (String args[]){
+
+            Student s = new Student();
+        System.out.println(s.get_verified());
+        s.set_membership("Gold");
+        System.out.println(s.get_membership());
+        /*
         User.admins = new ArrayList<User>();
         User.admins.add(new User("Kunal"));
         User.admins.add(new User("kumkum"));
         User.admins.add(new User("dear"));
 
-        User.print_admin_names();
+        User.print_admin_names();*/
+
+
             }
 }
